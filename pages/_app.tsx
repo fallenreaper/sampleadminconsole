b20f4a274f4fa409d127a1e8600d006c94e396c9
fallenreaper <wllm.francis@gmail.com>
2022-11-22 23:22:00 -0500

    Update packages and added some page defs and a profile image sample

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { CacheProvider, EmotionCache, ThemeProvider } from '@emotion/react'
-import { ColorModeContext, useMode } from '../utilities/theme';
-import Head from 'next/head';
-import { CssBaseline } from '@mui/material';
-import createEmotionCache from '../utilities/createEmotionCache';
-import TopBar from '../components/topbar';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache, ThemeProvider } from "@emotion/react";
+import { ColorModeContext, useMode } from "../utilities/theme";
+import Head from "next/head";
+import { CssBaseline } from "@mui/material";
+import createEmotionCache from "../utilities/createEmotionCache";
+import TopBar from "../components/topbar";
+import SideBar from "../components/sidebar";
+import { ProSidebarProvider} from "react-pro-sidebar"
 
 const clientSideEmotionCache = createEmotionCache();
 
 interface MyAppProps extends AppProps {
-  emotionCache ?: EmotionCache
+  emotionCache?: EmotionCache;
 }
 
-export default function App( props: MyAppProps) {
-  const [theme, colorMode] =  useMode();
+export default function App(props: MyAppProps) {
+  const [theme, colorMode] = useMode();
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className='app'>
-            <div className='content'>
-              
-              <TopBar />
-              <Component {...pageProps} />
+    <ProSidebarProvider>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="app">
+              <SideBar />
+              <div className="content">
+                <TopBar />
+                <Component {...pageProps} />
+              </div>
             </div>
-          </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
-    </CacheProvider>
-  )
+          </ThemeProvider>
+        </ColorModeContext.Provider>
+      </CacheProvider>
+    </ProSidebarProvider>
+  );
 }
