21203752c6a38bd0bffb8cfb32fb033de766fc1c
fallenreaper <wllm.francis@gmail.com>
2022-11-25 15:49:47 -0500

    Starting to test with Contact info

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,7 +1,97 @@
-
-
+import { Box, useTheme } from "@mui/material";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../utilities/theme";
+import { mockDataContacts } from "../mockData/mockData";
+import Header from "../components/header";
 
 export default function Contacts() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-  return (<div>Contacts Page</div>)
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 0.5
+    },
+    {
+      field: 'registrarId',
+      headerName: 'Registrar ID'
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      align: "left",
+      headerAlign: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+    },
+    {
+      field: "zipCode",
+      headerName: "Zip Code",
+      flex: 1,
+    }
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="CONTACTS" subtitle="List of Contacts for Future References" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[700],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[300],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[300],
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`
+          }
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataContacts} columns={columns} />
+      </Box>
+    </Box>
+  );
 }
