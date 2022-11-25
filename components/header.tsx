44579befbfb38336742b581622eb9340b8a168dc
fallenreaper <wllm.francis@gmail.com>
2022-11-25 00:33:00 -0500

    Created a Datagrid using the x-data-grid by mui

---
+++ components/header.tsx
@@ -0,0 +1,18 @@
+
+import { Typography, Box, useTheme } from "@mui/material"
+import { tokens } from '../utilities/theme' 
+
+const Header = ( data: { title: string, subtitle: string}) => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
+
+  return (
+    <Box mb="30px">
+      <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{mb: "5px"}}>{data.title.toUpperCase()}</Typography>
+      <Typography variant="h5" color={colors.greenAccent[400]}>{data.subtitle}</Typography>
+    </Box>
+  )
+}
+
+export default Header
--- pages/team.tsx
+++ pages/team.tsx
@@ -1,7 +1,109 @@
-
-
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { tokens } from "../utilities/theme";
+import { mockDataTeam } from "../mockData/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from "../components/header";
+import { borderRadius } from "@mui/system";
 
 export default function Teams() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-  return (<div>Teams Page</div>)
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "ID",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      align: "left",
+      headerAlign: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "access",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: (data: { row: { access: string } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            borderRadius="5px"
+            sx={{
+              backgroundColor:
+                data.row.access === "admin"
+                  ? colors.greenAccent[600]
+                  : colors.greenAccent[700]
+              
+            }}
+          >
+            {data.row.access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {data.row.access === "manager" && <SecurityOutlinedIcon />}
+            {data.row.access === "user" && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ml: "5px"}}>
+              {data.row.access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none"
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none"
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300]
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400]
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700]
+          }
+        }}
+      >
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
 }
