7bd3906a899aca0fa58af5578fa6369b4d4e69b1
fallenreaper <wllm.francis@gmail.com>
2022-11-26 21:20:18 -0500

    Flex to split events and calendar better

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,98 +1,105 @@
 import { useState } from "react";
-import FullCalendar, {formatDate} from "@fullcalendar/react"
+import FullCalendar, { formatDate } from "@fullcalendar/react";
 
 //plugins
-import dayGridPlugin from "@fullcalendar/daygrid"
-import timeGridPlugin from "@fullcalendar/timegrid"
-import interactionPlugin from "@fullcalendar/interaction"
-import listPlugin from "@fullcalendar/list"
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
 
 import {
   Box,
   List,
   ListItem,
   ListItemText,
-  Typography, 
-  useTheme
-} from "@mui/material"
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { tokens } from "../utilities/theme";
 import Header from "../components/header";
 import { EventApi } from "@fullcalendar/common";
 
-export default function Calendar () {
-  const theme = useTheme()
-  const colors = tokens(theme.palette.mode)
-  const [ currentEvents, setCurrentEvents] = useState<EventApi[]>([])
-  // const setCurrentEvents = (events: any[]) => {_setCurrentEvents(events)}
+export default function Calendar() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
   const handleDateClick = (selected: any) => {
-    const title = prompt("Enter the Title for your new Event")
+    const title = prompt("Enter the Title for your new Event");
     const calendarApi = selected.view.calendar;
-    calendarApi.unselect()
+    calendarApi.unselect();
     if (title) {
       calendarApi.addEvent({
         id: `${selected.dateStr}-${title}`,
         title,
         state: selected.startStr,
         end: selected.endStr,
-        allDay: selected.allDay
-      })
+        allDay: selected.allDay,
+      });
     }
-  }
+  };
 
-  const handleEventClick = (selected: any) => {
-    
-  }
-
+  const handleEventClick = (selected: any) => {};
 
   return (
     <Box m="20px">
       <Header title="CALENDAR" subtitle="Where Organization Happens" />
-      <Box>
+      <Box display="flex" justifyContent="space-between">
         {/* Calendar Area */}
-        <Box>
+        <Box
+          flex="1 1 20%"
+          sx={{
+            backgroundColor: colors.primary[600],
+          }}
+          p="15px"
+          borderRadius="4px"
+        >
           {/* Calendar Sidebar */}
-          <Box>
-            <Typography variant="h5">
-              Events
-            </Typography>
-            <List>
-              {currentEvents.map( (event: EventApi) => {
-                return (
-                  <ListItem id={event.id}>
-                    <ListItemText
-                      primary={event.title}
-                      secondary={
-                        <Typography>
-                        {
-                          event.start && formatDate(event.start, {
+          {/* <Box> */}
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event: EventApi) => {
+              return (
+                <ListItem
+                  id={event.id}
+                  sx={{
+                    backgroundColor: colors.greenAccent[500],
+                    m: "10px 0",
+                    borderRadius: "2px",
+                  }}
+                >
+                  <ListItemText
+                    primary={event.title}
+                    secondary={
+                      <Typography>
+                        {event.start &&
+                          formatDate(event.start, {
                             year: "numeric",
                             month: "short",
-                            day:"numeric"
-                          })
-                        }
+                            day: "numeric",
+                          })}
                       </Typography>
-                      } //End Secondary Tag
-                    />
-                  </ListItem>
-                )
-              })}
-            </List>
-          </Box>
+                    } //End Secondary Tag
+                  />
+                </ListItem>
+              );
+            })}
+          </List>
+          {/* </Box> */}
         </Box>
-        <Box>
+        <Box flex="1 1 100%">
           {/* Main Calendar Area */}
           <FullCalendar
-            height= "75vh"
+            height="75vh"
             plugins={[
               dayGridPlugin,
               timeGridPlugin,
               interactionPlugin,
-              listPlugin
+              listPlugin,
             ]}
             headerToolbar={{
-              left:"prev,next today",
+              left: "prev,next today",
               center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
             }}
             initialView="dayGridMonth"
             editable={true}
@@ -101,14 +108,11 @@ export default function Calendar () {
             dayMaxEvents={true}
             select={handleDateClick}
             eventClick={handleEventClick}
-            eventsSet={ (events) => setCurrentEvents(events) }
-            initialEvents={
-              []
-            }
-
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[]}
           />
         </Box>
       </Box>
     </Box>
-  )
+  );
 }
