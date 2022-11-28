921cb3218ac63baf222564939f50697bcbc3b386
fallenreaper <wllm.francis@gmail.com>
2022-11-27 20:59:40 -0500

    Added a border

--- pages/geography.tsx
+++ pages/geography.tsx
@@ -1,14 +1,18 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import Header from "../components/header";
 import GeographyChart from "../components/geography";
+import { tokens } from "../utilities/theme";
 
 
 const Geography = () => {
 
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Header title="Geography Chart" subtitle="Simple Geography Chart" />
-      <Box height="75vh">
+      <Box height="75vh" border={`1px solid ${colors.grey[900]}`} borderRadius="4px">
         <GeographyChart />
       </Box>
     </Box>
