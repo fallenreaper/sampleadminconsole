f5a615840c064bc186dec4004fdec6a146d24848
fallenreaper <wllm.francis@gmail.com>
2022-11-30 14:09:04 -0500

    Formatting

--- components/statbox.tsx
+++ components/statbox.tsx
@@ -3,13 +3,23 @@ import { tokens } from "../utilities/theme";
 import ProgressCircle from "./progresscircle";
 
 function StatBox({
-  title, subtitle, icon, progress, increase
-}: { title: string, subtitle: string, icon: any, progress: string, increase: string}) {
+  title,
+  subtitle,
+  icon,
+  progress,
+  increase,
+}: {
+  title: string;
+  subtitle: string;
+  icon: any;
+  progress: string;
+  increase: string;
+}) {
   let _progress = 0;
   try {
-    _progress = parseFloat(progress)
-  }catch (e){ 
-    console.log("Failed to Parse `progress` to number.  Defaulting to `0`")
+    _progress = parseFloat(progress);
+  } catch (e) {
+    console.log("Failed to Parse `progress` to number.  Defaulting to `0`");
   }
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
